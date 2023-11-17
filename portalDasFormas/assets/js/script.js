a2f2cfbbdb83f9ae693f05f72222c7173540de9c
werik-antunes <127903405+werik-antunes@users.noreply.github.com>
2023-11-17 18:49:45 -0300

    Add files via upload

---
+++ portalDasFormas/assets/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("botao-calcular-triangulo").addEventListener("click", function{
+
+
+})
+
+document.getElementById("botao-calcular-retangulo").addEventListener("click", function{
+
+    
+})
